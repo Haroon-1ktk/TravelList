8b7240f2986e93ef46204932f13aa77dbe512a77
Haroon-1ktk <121959975+Haroon-1ktk@users.noreply.github.com>
2024-05-09 10:13:34 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "jeans", quantity: 3, packed: true }
-];
-
 import React, { useState } from 'react'
 
 const App = () => {
